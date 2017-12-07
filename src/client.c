9a2b22d8b9710bc8189321d0094477fa29c240a9
Eloise BILLA <eloise.billa.ocre@cea.fr>
2017-12-07 17:16:46 +0100

    HelloWorld with Json messages

--- src/client.c
+++ src/client.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<czmq.h>
+#include<json.h>
+#include<string.h>
 
 int main (void)
 {
@@ -8,11 +10,41 @@ int main (void)
     //zsock_t *req = zsock_new_req("tcp://10.252.0.1:7410");
     /*pour ocre*/
     zsock_t *req = zsock_new_req("tcp://192.168.129.25:7410");
+  
+    //on cree une requete et on l'envoie
+    json_object *request = json_object_new_object();
+    json_object *str = json_object_new_string("Hello");
+    json_object_object_add(request, "req", str);
     
-    zstr_send(req, "hello");
+    const char* req_c = json_object_to_json_string(request);
+    zstr_send(req, req_c);
+
+    //on clean
+    json_object_object_del(request, "req");
+    json_object_put(request);
+
+    //on recupere la reponse
     char* string = zstr_recv(req);
-    puts(string);
+    //puts(string);
+    json_object *reply = json_tokener_parse(string);
     zstr_free(&string);
+
+    //on verifie la reponse
+    json_object *rep;
+    if(!json_object_object_get_ex(reply, "rep", &rep))
+        printf("Error: no key found\n");
+    if(strcmp(json_object_get_string(rep),"World")==0)
+        printf("Win client: World reçu !\n");
+    
+    //printf("%s %d",json_object_get_string(rep), json_object_get_string_len(rep));
+    
+    //on clean
+    json_object_object_del(reply, "req");
+    json_object_object_del(reply, "rep");
+    if(json_object_put(reply) != 1)
+        printf("error free reply");
+
     zsock_destroy(&req);
+
     return 0;
 }
--- src/server.c
+++ src/server.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <czmq.h>
+#include <json.h>
+#include <string.h>
 
 int main(void)
 {
@@ -9,11 +11,34 @@ int main(void)
     /*pour ocre*/
     zsock_t *rep = zsock_new_rep("tcp://192.168.129.25:7410");
     
+    //on recupere la requete
     char *string = zstr_recv(rep);
-    puts (string);
+    //puts (string);
+    json_object *request = json_tokener_parse(string);
     zstr_free(&string);
 
-    zstr_send(rep,"World");
+    //on verifie le contenu
+    json_object *req;
+    if(!json_object_object_get_ex(request, "req", &req))
+        printf("Error: no key found\n");
+    if(strcmp(json_object_get_string(req), "Hello")==0)
+        printf("Win serveur: Hello reçu !\n");
+
+    //printf("%s %d",json_object_get_string(req), json_object_get_string_len(req));
+    
+    //on cree la reponse et on l'envoie
+    json_object *reply = json_object_new_string("World");
+    json_object_object_add(request, "rep", reply);
+    
+    const char* rep_c = json_object_to_json_string(request); 
+    zstr_send(rep,rep_c);
+    
+    //on clean
+    json_object_object_del(request, "req");
+    json_object_object_del(request, "rep");
+    if(json_object_put(request) != 1)
+        printf("error free request");
+
     zsock_destroy(&rep);
 
     return 0;
