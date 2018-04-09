93fb137c34b5c994d63cb88a5006f6055da0f573
Eloise BILLA <eloise.billa.ocre@cea.fr>
2018-04-09 19:26:47 +0200

    adapt client to massive tests: it has to give a prefix and a number of request to execute

--- src/client.c
+++ src/client.c
@@ -6,11 +6,12 @@
 int main(int argc, char **argv)
 {
     if (argv[1] == NULL || argv[2] == NULL) {
-        printf("please give a key and data to store\n");
+        printf("please give a number of request and a key prefix\n");
         return -1;
     }
 
     int nb_servers = 4;
+    char *data = "a word";
 
     /*init context*/
     int rc;
@@ -22,12 +23,21 @@ int main(int argc, char **argv)
     }
 
     /*create pattern to request different key each time*/
+    char *key;
+    int i = 0;
+    
+    for(i = 0; i < atoi(argv[1]); i++) {
+        if (asprintf(&key, "%s%d",argv[2],i) == -1) {
+            int err = errno;
+            fprintf(stderr, "Client: generating key error:%s\n",strerror(err));
+            finalize_context();
+            return -1;
+        }
 
-    /**/
-
-    rc = request_create(argv[1], argv[2]);
-    if (rc != 0)
-        fprintf(stderr, "Request failed\n");
+        rc = request_create(key, data);
+        if (rc != 0)
+            fprintf(stderr, "Request failed\n");
+    }
 
     finalize_context();
     return 0;
