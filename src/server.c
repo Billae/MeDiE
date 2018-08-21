fb6558451ca5dd7ef7800f238bc856566b45f4cd
Eloise BILLA <eloise.billa.ocre@cea.fr>
2018-08-21 17:55:08 +0200

    catching error for distribution function (+fix style)

--- src/server.c
+++ src/server.c
@@ -19,20 +19,22 @@
 int main(int argc, char **argv)
 {
     /*ocre usage need different port for each server*/
-    if (argv[1] == NULL || strcmp(argv[1], "o") != 0 && strcmp(argv[1], "p") != 0) {
-        fprintf(stderr, "please give a server type o or p (and port if type is o)\n");
+    if (argv[1] == NULL || strcmp(argv[1], "o") != 0
+            && strcmp(argv[1], "p") != 0) {
+        fprintf(stderr,
+                "please give a server type o or p (and port if type is o)\n");
         return -1;
     }
 
     zsock_t *rep;
 
     if (strcmp(argv[1], "p") == 0) {
-    /*for pcocc VM*/
+        /*for pcocc VM*/
         rep = zsock_new_rep("tcp://0.0.0.0:7410");
     }
 
     else {
-    /*for ocre*/
+        /*for ocre*/
         if (argv[2] == NULL) {
             fprintf(stderr, "please give a server port\n");
             return -1;
@@ -46,11 +48,18 @@ int main(int argc, char **argv)
             return -1;
         }
     }
-    distribution_init();
+
+    int rc = distribution_init();
+    if (rc != 0) {
+        fprintf(stderr, "Server: distribution init failed\n");
+        return -1;
+    }
+
+    int global_rc;
 
 while (1) {
 
-    int global_rc = 1;
+    global_rc = 0;
 
     /*receiving request*/
     char *string = zstr_recv(rep);
@@ -58,8 +67,8 @@ while (1) {
     zstr_free(&string);
 
     /*call the distribution processing*/
-    distribution_post_receive(request);
-    
+    global_rc = distribution_post_receive(request);
+
     /*processing*/
     json_object *key;
     if (!json_object_object_get_ex(request, "key", &key))
@@ -67,16 +76,18 @@ while (1) {
 
     json_object *type;
     if (!json_object_object_get_ex(request, "reqType", &type))
-        fprintf(stderr, "Server: json extract error: no key \"reqType\" found\n");
+        fprintf(stderr,
+                "Server: json extract error: no key \"reqType\" found\n");
 
     enum req_type reqType = json_object_get_int(type);
 
     switch (reqType) {
 
-    case RT_CREATE: //create
+    case RT_CREATE: /*create*/
     {   json_object *data;
         if (!json_object_object_get_ex(request, "data", &data))
-            fprintf(stderr, "Server: json extract error: no key \"data\" found\n");
+            fprintf(stderr,
+                    "Server: json extract error: no key \"data\" found\n");
         int rc;
         rc = generic_put(json_object_get_string(data),
                 json_object_get_string(key));
@@ -87,25 +98,25 @@ while (1) {
         }
     }
 
-    case RT_UPDATE: //update
+    case RT_UPDATE: /*update*/
         break;
 
-    case RT_DELETE: //delete
+    case RT_DELETE: /*delete*/
         break;
 
-    default: //get
+    default: /*get*/
         break;
     }
 
+
     /*creating reply and send*/
 
-    distribution_pre_send(request);
+    global_rc = distribution_pre_send(request);
     json_object *repFlag;
-    if (global_rc == 1)
+    if (global_rc == 0)
         repFlag = json_object_new_string("done");
     else
         repFlag = json_object_new_string("aborted");
-
 
     json_object_object_add(request, "repFlag", repFlag);
 
@@ -117,7 +128,10 @@ while (1) {
         fprintf(stderr, "Server: free request error\n");
 }
 
-    distribution_finalize();
+    rc = distribution_finalize();
+    if (rc != 0)
+        fprintf(stderr, "Server: distribution finalize failed\n");
+
     zsock_destroy(&rep);
     return 0;
 }
