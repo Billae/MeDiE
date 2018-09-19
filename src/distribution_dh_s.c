ec30b1bafce1fcdcd0cd69d80671872ee076c538
Eloise BILLA <eloise.billa.ocre@cea.fr>
2018-09-19 16:16:49 +0200

    fix last commit -_-

--- src/distribution_dh_s.c
+++ src/distribution_dh_s.c
@@ -150,10 +150,10 @@ int distribution_post_receive(json_object *request)
 
     int rc = mlt_get_entry(&table, json_object_get_int(entry), &id_srv, &ver);
 
-    if (rc != 0)
-        int err = errno;
-        fprintf(stderr, "Distribution:post_receive: mlt get entry failed: %s\n", strerror(err));
+    if (rc != 0) {
+        fprintf(stderr, "Distribution:post_receive: mlt get entry failed: %s\n", strerror(rc));
         return -1;
+    }
 
     json_object *ver_flag;
 
