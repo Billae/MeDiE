63c5d7361db7baa069a442e7fef77c6e6c45df7c
Eloise BILLA <eloise.billa.ocre@cea.fr>
2018-12-19 18:05:20 +0100

    change the percentage into cumulative percentage

--- benchmarks/load/load_to_percentage.py
+++ benchmarks/load/load_to_percentage.py
@@ -7,19 +7,25 @@ import matplotlib.pyplot as plt
 df = pd.read_csv(sys.argv[1], delimiter=';', dtype='float32')
 #n is the number of lines
 n = len(df)
+#n_srv is the number of columns
+n_srv = len(df.columns)
 
+cumuls = n_srv * [0]
 totals = n * [0]
+total_all = 0
+
 for i in range(0, n):
-    for e in df:
-        totals[i] += df[e][i]
+    for e in range(0, n_srv):
+        totals[i] += df.iat[i,e]
 
 #s = ';'.join(list(df)) + ';total' + '\n'
 s = ''
 for i in range(0, n):
-    for e in df:
-        s += str((df[e][i]/totals[i])*100) + ';'
-    s += str(totals[i]) + '\n'
-
+    total_all += totals[i]
+    for e in range(0, n_srv):
+        cumuls[e] += df.iat[i,e]
+        s += str((cumuls[e]/total_all)*100) + ';'
+    s += str(total_all) + '\n'
 with open('results.csv', 'w') as fh:
     fh.write(s)
 
