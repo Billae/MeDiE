2c4dc9776095d96bc3a08c30a993d11d2c4685a5
Eloise <billae@r3login.c-inti.ccc.ocre.cea.fr>
2019-04-18 10:18:50 +0200

    fusion of all post_process_*.sh in one with an additionnal argument

--- benchmarks/scripts/post_process.sh
+++ benchmarks/scripts/post_process.sh
@@ -5,13 +5,14 @@
 module load python3
 module load gnuplot
 
-if ([ -z $1 ])
+if ([ -z $1 ] || [ -z $2 ])
 then
-    echo "please give a path"
+    echo "please give a path and the type of run"
     exit 1 
 fi
 
 path=$1
+run=$2
 
 LOG_FILES=$(ls $path/server/load*)
 RES_FILE=$path/servers_load.csv
@@ -22,5 +23,5 @@ paste -d ';' $LOG_FILES >> $RES_FILE
 python3 scripts/server_post_process.py $path $RES_FILE
 python3 scripts/percentage_to_error.py $path
 
-gnuplot -c scripts/display_load.gnu $path dh
-gnuplot -c scripts/display_percentage_load.gnu $path dh
+gnuplot -c scripts/display_load.gnu $path $run
+gnuplot -c scripts/display_percentage_load.gnu $path $run
