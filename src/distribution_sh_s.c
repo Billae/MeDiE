46c065bbe0e2aab06d59cb39e8482ce9d2722912
Eloise BILLA <eloise.billa.ocre@cea.fr>
2019-05-06 17:43:25 +0200

    fix compilation issues

--- src/distribution_sh_s.c
+++ src/distribution_sh_s.c
@@ -1,5 +1,8 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include <errno.h>
 #include <string.h>
 #include <json.h>
@@ -8,8 +11,60 @@
 #include "murmur3.h"
 
 
+/*Each server has its own id_srv*/
+static int id_srv_self;
+
+
+#define max_id_size 21
+
+/* path in pcocc*/
+#define SRV_PATH "/home/billae/prototype_MDS/etc/server.cfg"
+/*path in ocre*/
+//#define SRV_PATH "/ccc/home/cont001/ocre/billae/prototype_MDS/etc/server.cfg"
+
+/* path in pcocc*/
+#define SCRATCH "/media/tmp_ack/sh/"
+/*path in ocre*/
+//#define SCRATCH "/ccc/home/cont001/ocre/billae/prototype_MDS/tmp/"
+
+
 int distribution_init()
 {
+    /*getting id_srv_self*/
+    char *id_srv;
+    id_srv = malloc(max_id_size*sizeof(*id_srv));
+    if (id_srv == NULL) {
+        int err = errno;
+        fprintf(stderr, "Distribution:init: id_srv malloc error: %s\n",
+            strerror(err));
+        return 0;
+    }
+
+    FILE *fd = fopen(SRV_PATH, "r");
+    if (fd == NULL) {
+        int err = errno;
+        fprintf(stderr, "Distribution:init: open server.cfg file failed: %s\n",
+            strerror(err));
+        free(id_srv);
+        return -1;
+    }
+    if (fgets(id_srv, max_id_size, fd) == NULL) {
+        fprintf(stderr, "Distribution:init: read config file failed\n");
+        free(id_srv);
+        return -1;
+    }
+    char *positionEntree = strchr(id_srv, '\n');
+    if (positionEntree != NULL)
+        *positionEntree = '\0';
+
+    char *value = strstr(id_srv, "ID");
+    value = strchr(value, '=');
+    value++;
+    id_srv_self = atoi(value);
+
+    free(id_srv);
+    fclose(fd);
+
     return 0;
 }
 
