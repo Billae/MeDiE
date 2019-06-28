8ee9cf2ae7469d2be9c4ac065e1d5df4bc2fa779
Eloise <billae@r3login.c-inti.ccc.ocre.cea.fr>
2019-06-28 17:29:54 +0200

    dh curves were false:2.5h rebalancing instead of 1h. Fix + add curve of reboot_clean

---
+++ benchmarks/scripts/analyse_load.py
@@ -0,0 +1,37 @@
+# fit a beta distribution on the data in order to get an estimate of the minimum and maximum (support of the distribution)
+# https://en.wikipedia.org/wiki/Beta_distribution
+# https://stats.stackexchange.com/questions/68983/beta-distribution-fitting-in-scipy
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.beta.html
+from scipy.stats import beta
+import seaborn as sns
+import matplotlib.pyplot as plot
+import numpy
+
+#prepare data
+data = numpy.loadtxt(open("real_traces/sh/sklb/12_clients/servers_load.csv", "rb"), delimiter=";", skiprows=1) # use skiprows if you have an header
+means = numpy.mean(data, axis=1) #mean of all servers
+requests = means/300 #5*60 #in requests per second
+
+
+# generate data for test
+#a, b, loc, scale = 2.31, 0.627, 5, 10
+#data = beta.rvs(a, b, loc, scale, size=1000)
+
+# plot the distribution of the data
+sns.set(color_codes=True)
+sns.distplot(requests, hist=False, rug=True)
+
+# fit beta distribution on data
+#(a, b, loc, scale) = beta.fit(requests, loc=0)
+# get minimum and maximum
+#minimum = loc
+#maximum = loc + scale
+
+m = numpy.mean(requests)
+s = numpy.std(requests)
+minimum = m - 4*s
+maximum = m + 4*s
+
+print (minimum, maximum)
+
+plot.show()
--- benchmarks/scripts/post_process.sh
+++ benchmarks/scripts/post_process.sh
@@ -20,11 +20,11 @@ RES_FILE=$path/servers_load.csv
 echo ${LOG_FILES} | tr ' ' ';' > $RES_FILE
 paste -d ';' $LOG_FILES >> $RES_FILE
 
-if ([ $run == "dh" ])
-then
+#if ([ $run == "dh" ])
+#then
     #8 is the number of line to delete because of the run script ends after rebalancing
-    sed -i -e :a -e '$d;N;2,8ba' -e 'P;D' $RES_FILE
-fi
+#    sed -i -e :a -e '$d;N;2,8ba' -e 'P;D' $RES_FILE
+#fi
 
 python3 scripts/server_post_process.py $path $RES_FILE
 python3 scripts/percentage_to_error.py $path
