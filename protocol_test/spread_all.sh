4962f397ed07f253c672bf69e8f3b060b3f40fd1
Eloise <billae@r3login.c-inti.ccc.ocre.cea.fr>
2019-11-14 12:06:34 +0100

    trace file is also an argument

--- protocol_test/spread_all.sh
+++ protocol_test/spread_all.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 
-if [ -z $1 ] || [ -z $2 ]
+if [ -z $1 ] || [ -z $2 ] || [ -z $3 ]
 then
-    echo "please give a number of steps and clients"
+    echo "please give the file path, a number of steps and clients"
     exit 1
 fi
 
-n_step=$1
-n_client=$2
+file=$1
+n_step=$2
+n_client=$3
 
 for((turn = 0; turn < n_step; turn++))
 do
-    python3 protocol_test/spread.py $SCRATCHDIR/scratch_vm/traces/generated/traces-$(($turn)).csv $n_client
+    python3 protocol_test/spread.py $file-$(($turn)).csv $n_client
 done
