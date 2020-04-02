758be5c6a0af3a15d9337b16955a9fbcdf091b07
Eloise <billae@r3login.c-inti.ccc.ocre.cea.fr>
2020-04-02 18:27:49 +0200

    add flux in path, add a job directory to create jobs, add a script to run theses jobs

--- job_generator.sh
+++ job_generator.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+if [ -z $1 ]
+    echo "Please give a job dir"
+
+job_dir=$1
+mkdir -p $job_dir
+
 gen_job () {
-jobfile="jobs/${runpath//\//_}.sh"
+jobfile="$job_dir${runpath//\//_}.sh"
 cat << EOF > $jobfile
     #!/bin/bash
-    jobid=\$(pcocc batch -p haswell --qos=test -t $((2*60)) -c 4 all:17 | cut -d ' ' -f 4)
+    jobid=\$(pcocc batch -p haswell -t $((2*60)) -c 4 all:17 | cut -d ' ' -f 4)
     until [[ "\$(squeue -j \$jobid -o %T | tail -n 1)" = "RUNNING" ]]; do
         sleep 60
     done
@@ -13,9 +19,9 @@ cat << EOF > $jobfile
         pcocc ssh -j \$jobid vm\$j "make -j 4 -f prototype_MDS/Makefile_${method} CPPFLAGS=\"$def\"" &
     done
     wait
-    echo "Compiling finished"
+    echo "Compiling job \$jobid finished"
     pcocc ssh -j \$jobid vm16 mkdir -p $logpath
-    pcocc ssh -j \$jobid vm16 "./prototype_MDS/launch_all.sh 4 12 $runpath $method /mnt/scratch/traces/real/5min/12_clients/changelog 36 r $redistrib &> $logpath/\$(date -I).log &" 
+    pcocc ssh -j \$jobid vm16 "./prototype_MDS/launch_all.sh 4 12 $runpath $method $flux_param $redistrib &> $logpath/\$(date -I).log &" 
 EOF
 chmod +x $jobfile
 }
@@ -34,11 +40,13 @@ run_job () {
     pcocc ssh -j $jobid vm16 "./prototype_MDS/launch_all.sh 4 12 $runpath $method /mnt/scratch/traces/real/5min/12_clients/changelog 292 r $redistrib &> $logpath/$(date -I).log &" 
 }
 
+flux=real_3h
+flux_param="/mnt/scratch/traces/real/5min/12_clients/changelog 36 r"
 method=dh
 for alpha in 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1; do
     for nentry in 50 100 500 1000 5000 10000; do
         for redistrib in 2 6 12 24 60; do
-            runpath=method_$method/alpha_$alpha/nentry_$nentry/rebalancing_$redistrib/
+            runpath=flux_$flux/method_$method/alpha_$alpha/nentry_$nentry/rebalancing_$redistrib/
             logpath=/mnt/scratch/logs/$runpath
 
             # all escapes character to give " to source code
@@ -48,8 +56,6 @@ for alpha in 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1; do
             def="-DALPHA=$alpha -DN_ENTRY=$nentry -DSCRATCH=\\\\\\\\\\\\\\\"/mnt/scratch/tmp_ack/$runpath\\\\\\\\\\\\\\\" -DPREFIX=\\\\\\\\\\\\\\\"/mnt/result/$runpath\\\\\\\\\\\\\\\" "
             #run_job
             gen_job
-            #for splitting jobs in file of 100 lines
-            ls jobs/method_* | tr " " "\n" | split -l 100 - jobs/run_job_
         done
     done
 done
---
+++ run_jobs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#this script has to be launched with the nohup option !
+
+if [ -d $1 ]
+    echo "Please give a valid job dir"
+
+job_dir=$1
+
+#launch all jobs in the job list run_jobxx
+for job in $(ls $job_dir)
+do
+    $job &> $file.log
+done
